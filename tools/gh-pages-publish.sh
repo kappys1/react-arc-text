85f348d6fb91ff6c1d2e06640a4d5721b54b563a
alex marcos <alexsbd1@gmail.com>
2019-07-18 13:53:48 +0200

    ci(script): fix script

--- tools/gh-pages-publish.sh
+++ tools/gh-pages-publish.sh
@@ -2,12 +2,7 @@
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
-rm -rf public
-mkdir public
 
-# config
-git config --global user.email "alexsbd1@gmail.com"
-git config --global user.name "alex marcos - Travis CI"
 
 # build (CHANGE THIS)
 
@@ -15,5 +10,10 @@ git config --global user.name "alex marcos - Travis CI"
 cd example/build
 git init
 git add .
+
+# config
+git config --global user.email "alexsbd1@gmail.com"
+git config --global user.name "alex marcos"
+
 git commit -m "docs(docs): Deploy to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GITHUB_REPO}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GITHUB_REPO}" master:gh-pages
