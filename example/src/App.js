e250574d57dd76ca0e6e0decc883a7d3c580a72f
alex marcos <alexsbd1@gmail.com>
2019-07-18 11:58:36 +0200

    doc(): create docs page

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,69 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 
-import ExampleComponent from 'react-arc-text'
+import ReactArcText from 'react-arc-text'
+import './App.scss'
 
 const App = () =>  {
+  const title = 'react-arc-text'
 
+  const [text, setText] = useState('aa');
+  const [arc, setArc] = useState(150);
+  const [classFont, setClassFont] = useState('');
+  const classes = [
+    { name: 'Roboto', value: 'Roboto' },
+    { name: 'Roboto Mono', value: 'Roboto-mono' },
+    { name: 'Montserrat', value: 'Montserrat' },
+    { name: 'Mountains of Christmas', value: 'Mountains' }
+  ]
+  const [direction, setDirection] = useState(1);
+  const directions = [
+    { name: 'clockwise', value: 1 },
+    { name: 'counter-clockwise', value: -1 }
+  ]
 
-  const [text, setText] = useState('');
 
   return (
-    <div>
-      <ExampleComponent text={text} />
+    <div  className="Page">
+      <nav>
+        <h1>{title}</h1>
+      </nav>
+
+      <section>
+        <div className={'show ' + classFont}>
+          <ReactArcText text={text} direction={direction} arc={arc} class={classFont} />
+        </div>
+
+        <div className="content">
+          <div className="content-row">
+            <div className="form-group">
+              <label>Text</label>
+              <input value={text}  onChange={e => setText(e.target.value)} type="text" />
+            </div>
+            <div className="form-group">
+              <label>Arc</label>
+              <input value={arc} onChange={e => setArc(+e.target.value)} type="number"/>
+            </div>
+          </div>
+          <div className="content-row">
+            <div className="form-group">
+              <label>Text</label>
+              <select className="custom-select" onChange={e => setClassFont(e.target.value)}>
+                <option value="" disabled>Choose your font-family</option>
+                {classes.map(c => <option key={c.value} value={c.value} >{c.name}</option>)}
+              </select>
+            </div>
+            <div className="form-group">
+              <label>Text</label>
+                <select className="custom-select" onChange={e => setDirection(+e.target.value)}>
+                <option value="" disabled>Choose the direction</option>
+                {directions.map((direction) => <option key={direction.value} value={direction.value} >{direction.name}</option>)}
+              </select>
+            </div>
     </div>
+        </div>
+      </section>
+    </div>
+
   )
 
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types'
 
 import ArcText from 'arc-text'
 
-export default class ExampleComponent extends Component {
+export default class ReactArcText extends Component {
 
   static propTypes = {
     text: PropTypes.string,
+    class: PropTypes.string,
     direction: PropTypes.number,
     arc: PropTypes.number
   }
@@ -15,27 +16,31 @@ export default class ExampleComponent extends Component {
     this.text = this.props.text || ''
     this.direction = this.props.direction || 1
     this.arc = this.props.arc || 150
+    this.class = this.props.class || ''
   }
 
   arcLetters = () => {
-    if (this.textCyrcle) {
-      this.textCyrcle.destroy()
-    }
+    this.checkProps()
+    if (this.container) {
+      if (this.textCyrcle) {
+        this.textCyrcle.destroy()
+      }
 
-    this.container.innerHTML = this.text
-    this.textCyrcle = new ArcText(this.container)
-    this.textCyrcle.arc(this.arc)
-    this.textCyrcle.direction(this.direction)
+      this.container.innerHTML = this.text
+      this.textCyrcle = new ArcText(this.container)
+      this.textCyrcle.arc(this.arc)
+      this.textCyrcle.direction(this.direction)
+    }
   }
 
   componentDidMount() {
-    this.checkProps()
     this.arcLetters()
   }
 
   render() {
+    this.arcLetters()
     return (
-      <div ref={(el) => { this.container = el }} />
+      <div className={'react-arc-text ' + this.class} ref={(el) => { this.container = el }} />
     )
   }
 }
